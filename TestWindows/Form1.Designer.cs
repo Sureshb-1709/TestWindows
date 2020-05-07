242aa14bed7795401096c78f283685d48f7d22ed
surmca17@outlook.com <surmca17@outlook.com>
2020-05-07 15:05:32 +0530

    Add project files.

---
+++ TestWindows/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TestWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCut
+            // 
+            this.btnCut.Location = new System.Drawing.Point(53, 74);
+            this.btnCut.Name = "btnCut";
+            this.btnCut.Size = new System.Drawing.Size(75, 23);
+            this.btnCut.TabIndex = 0;
+            this.btnCut.Text = "Cut";
+            this.btnCut.UseVisualStyleBackColor = true;
+            this.btnCut.Click += new System.EventHandler(this.btnCut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 383);
+            this.Controls.Add(this.btnCut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCut;
+    }
+}
+
---
+++ TestWindows/Form1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCut_Click(object sender, EventArgs e)
+        {
+            Message(btnCut.Text);
+        }
+
+
+        private void Message(string msg)
+        {
+            IHubProxy _hub;
+            string url = @"http://localhost:59099/";
+            var connection = new HubConnection(url);
+            _hub = connection.CreateHubProxy("employeeHub");
+            connection.Start().Wait();
+
+            //_hub.On("ReceiveLength", x => Console.WriteLine("Cut"));
+
+            string line = msg;
+            if (msg != null)
+            {
+                _hub.Invoke("sendNotifications", line).Wait();
+            }
+        }
+    }
+}
